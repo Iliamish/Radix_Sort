2e93d38531ee1a4322c54e5e93d1db3ebc230e94
Iliamish <qazcdeedczaq123>
2020-10-17 15:21:00 +0300

    add tests

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 RESULTS:
-Sequential STL container (n = 4000000): 71.253
-Sequential STL container std::sort (n = 4000000): 12.895
-Sequential dynamic array (n = 4000000): 11.381
+
+Sequential STL container (n = 4000000): 79.395
+
+Sequential STL container std::sort (n = 4000000): 14.642
+
+Sequential dynamic array (n = 4000000): 12.651
+
--- main.cpp
+++ main.cpp
@@ -12,13 +12,13 @@
 TEST_CASE("Sequential STL container", "[classic]"){
 	const size_t N = 4000000;
 	std::ofstream outfile("Readme.md");
-	outfile << "RESULTS:" << std::endl;
+	outfile << "RESULTS:\n" << std::endl;
 
 	clock_t begin_time = clock();
 	std::vector<int> vec = generators::generateRandomNumbersContainer<std::vector<int>>(N, 0, 99999);
 	sort::radix_sort_stl_containers(vec, 5, 10);
 	std::cout << float(clock() - begin_time) / CLOCKS_PER_SEC << std::endl;
-	outfile << "Sequential STL container (n = " << N << "): " << float(clock() - begin_time) / CLOCKS_PER_SEC << std::endl;
+	outfile << "Sequential STL container (n = " << N << "): " << float(clock() - begin_time) / CLOCKS_PER_SEC << "\n" << std::endl;
 
 	outfile.close();
 
@@ -33,7 +33,7 @@ TEST_CASE("Sequential STL container std::sort", "[classic]") {
 	std::vector<int> vec = generators::generateRandomNumbersContainer<std::vector<int>>(N, 0, 99999);
 	std::sort(vec.begin(), vec.end());
 	std::cout << float(clock() - begin_time) / CLOCKS_PER_SEC << std::endl;
-	outfile << "Sequential STL container std::sort (n = " << N << "): " << float(clock() - begin_time) / CLOCKS_PER_SEC << std::endl;
+	outfile << "Sequential STL container std::sort (n = " << N << "): " << float(clock() - begin_time) / CLOCKS_PER_SEC << "\n" << std::endl;
 
 	outfile.close();
 
@@ -48,7 +48,7 @@ TEST_CASE("Sequential dynamic array( N = 4000000)", "[classic]") {
 	int* arr = generators::generateRandomNumbersArray<int>(N, 0, 99999);
 	int* out = sort::radix_sort_arrays<int>(arr, N, 5, 10);
 	std::cout << float(clock() - begin_time) / CLOCKS_PER_SEC << std::endl;
-	outfile << "Sequential dynamic array (n = " << N << "): " << float(clock() - begin_time) / CLOCKS_PER_SEC << std::endl;
+	outfile << "Sequential dynamic array (n = " << N << "): " << float(clock() - begin_time) / CLOCKS_PER_SEC << "\n" << std::endl;
 
 	outfile.close();
 
